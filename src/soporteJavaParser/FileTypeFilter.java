e3de9b00def689a99a5ebc911c43068f6c8f2650
cristian <xcrisflores12@gmail.com>
2018-06-01 16:50:57 -0300

    pantalla principal

---
+++ src/soporteJavaParser/FileTypeFilter.java
@@ -0,0 +1,27 @@
+
+import java.io.File;
+import javax.swing.filechooser.*;
+
+public class FileTypeFilter extends FileFilter {
+	
+	private final String extension;
+	private final String descripcion;
+	
+	public FileTypeFilter(String extencion,String descripcion) {
+		this.descripcion = descripcion;
+		this.extension = extencion;
+	}
+	@Override
+	public boolean accept(File f) {
+		if (f.isDirectory()) {
+			return true;
+		}
+		return f.getName().endsWith(extension);
+	}
+
+	@Override
+	public String getDescription() {
+		return descripcion + String.format(" (*%s)", extension);
+	}
+
+}
