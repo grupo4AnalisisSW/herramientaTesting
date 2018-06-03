11e48e76d3b5a4bcf2591a7a355f390a6a695957
Alexis Aranda Ocampo <alexis_aranda_ocampo@hotmail.com>
2018-06-03 19:37:49 -0300

    Pantalla final_final___finalVerdadero2.1_fifinal

--- src/pantalla/PantallaPrincipal.java
+++ src/pantalla/PantallaPrincipal.java
@@ -58,7 +58,7 @@ public class PantallaPrincipal extends JFrame {
 	public PantallaPrincipal() {
 		setTitle("Herramienta de Testing");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 800, 550);
+		setBounds(100, 100, 800, 584);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -78,7 +78,7 @@ public class PantallaPrincipal extends JFrame {
 				//codigo.setText(listMetodos.getSelectedItem());
 			}
 		});
-		listMetodos.setBounds(401, 226, 369, 135);
+		listMetodos.setBounds(401, 265, 369, 135);
 		contentPane.add(listMetodos);
 		
 		List listClases = new List();
@@ -89,7 +89,7 @@ public class PantallaPrincipal extends JFrame {
 				listMetodos.add(listClases.getSelectedItem());
 			}
 		});
-		listClases.setBounds(10, 228, 362, 135);
+		listClases.setBounds(10, 267, 362, 135);
 		contentPane.add(listClases);
 		
 		List listArchivos = new List();
@@ -100,7 +100,7 @@ public class PantallaPrincipal extends JFrame {
 				listClases.add(listArchivos.getSelectedItem());
 			}
 		});
-		listArchivos.setBounds(10, 57, 362, 130);
+		listArchivos.setBounds(6, 77, 362, 130);
 		contentPane.add(listArchivos);
 		
 		JMenuItem mntmAbrir = new JMenuItem("Abrir directorio");
@@ -140,102 +140,116 @@ public class PantallaPrincipal extends JFrame {
 		JLabel lblNewLabel = new JLabel("Seleccione un archivo de la lista:");
 		lblNewLabel.setForeground(new Color(178, 34, 34));
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblNewLabel.setBounds(10, 35, 276, 16);
+		lblNewLabel.setBounds(6, 55, 276, 16);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblSeleccioneUnaClase = new JLabel("Seleccione una clase de la lista:");
 		lblSeleccioneUnaClase.setForeground(new Color(178, 34, 34));
 		lblSeleccioneUnaClase.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblSeleccioneUnaClase.setBounds(10, 206, 362, 16);
+		lblSeleccioneUnaClase.setBounds(10, 245, 362, 16);
 		contentPane.add(lblSeleccioneUnaClase);
 		
 		JLabel lblSeleccioneUnMetodo = new JLabel("Seleccione un metodo de la lista:");
 		lblSeleccioneUnMetodo.setForeground(new Color(178, 34, 34));
 		lblSeleccioneUnMetodo.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblSeleccioneUnMetodo.setBounds(401, 206, 369, 16);
+		lblSeleccioneUnMetodo.setBounds(401, 245, 369, 16);
 		contentPane.add(lblSeleccioneUnMetodo);
 		
 		JLabel lblLineasDeCdigo = new JLabel("Lineas de c\u00F3digo del archivo:");
 		lblLineasDeCdigo.setHorizontalAlignment(SwingConstants.CENTER);
 		lblLineasDeCdigo.setForeground(new Color(178, 34, 34));
 		lblLineasDeCdigo.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblLineasDeCdigo.setBounds(401, 57, 369, 16);
+		lblLineasDeCdigo.setBounds(397, 77, 369, 16);
 		contentPane.add(lblLineasDeCdigo);
 		
 		JLabel lblPorcentajeDeLineas = new JLabel("Porcentaje de lineas de c\u00F3digo del archivo comentadas:");
 		lblPorcentajeDeLineas.setHorizontalAlignment(SwingConstants.CENTER);
 		lblPorcentajeDeLineas.setForeground(new Color(178, 34, 34));
 		lblPorcentajeDeLineas.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblPorcentajeDeLineas.setBounds(401, 125, 369, 16);
+		lblPorcentajeDeLineas.setBounds(397, 145, 369, 16);
 		contentPane.add(lblPorcentajeDeLineas);
 		
 		JLabel lblFanIn = new JLabel("Fan In:");
 		lblFanIn.setHorizontalAlignment(SwingConstants.CENTER);
 		lblFanIn.setForeground(new Color(178, 34, 34));
 		lblFanIn.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblFanIn.setBounds(10, 374, 362, 16);
+		lblFanIn.setBounds(10, 413, 362, 16);
 		contentPane.add(lblFanIn);
 		
 		JLabel lblLongitudDeHalstead = new JLabel("Longitud de Halstead:");
 		lblLongitudDeHalstead.setHorizontalAlignment(SwingConstants.CENTER);
 		lblLongitudDeHalstead.setForeground(new Color(178, 34, 34));
 		lblLongitudDeHalstead.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblLongitudDeHalstead.setBounds(401, 376, 369, 16);
+		lblLongitudDeHalstead.setBounds(401, 415, 369, 16);
 		contentPane.add(lblLongitudDeHalstead);
 		
 		JLabel lblFanOut = new JLabel("Fan Out:");
 		lblFanOut.setHorizontalAlignment(SwingConstants.CENTER);
 		lblFanOut.setForeground(new Color(178, 34, 34));
 		lblFanOut.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblFanOut.setBounds(10, 438, 362, 16);
+		lblFanOut.setBounds(10, 477, 362, 16);
 		contentPane.add(lblFanOut);
 		
 		JLabel lblVolumenDeHalstead = new JLabel("Volumen de Halstead:");
 		lblVolumenDeHalstead.setHorizontalAlignment(SwingConstants.CENTER);
 		lblVolumenDeHalstead.setForeground(new Color(178, 34, 34));
 		lblVolumenDeHalstead.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblVolumenDeHalstead.setBounds(401, 438, 369, 16);
+		lblVolumenDeHalstead.setBounds(401, 477, 369, 16);
 		contentPane.add(lblVolumenDeHalstead);
 		
 		JLabel lblNewLabel_1 = new JLabel("[Cant Lineas]");
 		lblNewLabel_1.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 15));
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_1.setBounds(401, 84, 369, 30);
+		lblNewLabel_1.setBounds(397, 104, 369, 30);
 		contentPane.add(lblNewLabel_1);
 		
 		JLabel label = new JLabel("[Cant Lineas]%");
 		label.setHorizontalAlignment(SwingConstants.CENTER);
 		label.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 15));
-		label.setBounds(401, 152, 369, 30);
+		label.setBounds(397, 172, 369, 30);
 		contentPane.add(label);
 		
 		JLabel lblfanIn = new JLabel("[FAN IN]");
 		lblfanIn.setHorizontalAlignment(SwingConstants.CENTER);
 		lblfanIn.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 15));
-		lblfanIn.setBounds(10, 401, 362, 30);
+		lblfanIn.setBounds(10, 440, 362, 30);
 		contentPane.add(lblfanIn);
 		
 		JLabel lblfanOut = new JLabel("[FAN OUT]");
 		lblfanOut.setHorizontalAlignment(SwingConstants.CENTER);
 		lblfanOut.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 15));
-		lblfanOut.setBounds(10, 465, 362, 30);
+		lblfanOut.setBounds(10, 504, 362, 30);
 		contentPane.add(lblfanOut);
 		
 		JLabel lbllong = new JLabel("[LONG]");
 		lbllong.setHorizontalAlignment(SwingConstants.CENTER);
 		lbllong.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 15));
-		lbllong.setBounds(401, 401, 369, 30);
+		lbllong.setBounds(401, 440, 369, 30);
 		contentPane.add(lbllong);
 		
 		JLabel lblvol = new JLabel("[VOL]");
 		lblvol.setHorizontalAlignment(SwingConstants.CENTER);
 		lblvol.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 15));
-		lblvol.setBounds(401, 465, 369, 30);
+		lblvol.setBounds(401, 504, 369, 30);
 		contentPane.add(lblvol);
 		
 		JSeparator separator = new JSeparator();
-		separator.setBounds(10, 193, 764, 2);
+		separator.setBounds(6, 213, 764, 2);
 		contentPane.add(separator);
+		
+		JLabel lblNewLabel_2 = new JLabel("Archivos .java");
+		lblNewLabel_2.setForeground(new Color(0, 0, 128));
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_2.setFont(new Font("Times New Roman", Font.BOLD, 17));
+		lblNewLabel_2.setBounds(10, 33, 760, 26);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblClasesYMetodos = new JLabel("Clases y metodos");
+		lblClasesYMetodos.setHorizontalAlignment(SwingConstants.CENTER);
+		lblClasesYMetodos.setForeground(new Color(0, 0, 128));
+		lblClasesYMetodos.setFont(new Font("Times New Roman", Font.BOLD, 17));
+		lblClasesYMetodos.setBounds(10, 221, 760, 26);
+		contentPane.add(lblClasesYMetodos);
 		
 	}
 }
